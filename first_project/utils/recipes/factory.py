842d6a5dc21df62dbfb59b90c86a0f1ea4ace2f1
João Lucas <Jlucasoc@hotmail.com>
2022-07-31 18:40:44 -0300

    Using app_name in urls 📛

--- first_project/utils/recipes/factory.py
+++ first_project/utils/recipes/factory.py
@@ -14,6 +14,7 @@ def make_recipe():
     width, height = rand_ratio()
 
     return {
+        "id": randint(1, 100),
         "title": fake.sentence(nb_words=6),
         "description": fake.sentence(nb_words=12),
         "preparation_time": fake.random_number(digits=2, fix_len=True),
