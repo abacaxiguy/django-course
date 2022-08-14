57bb1978fef9a97c3ffdc7a1b536d05d29844f86
João Lucas <Jlucasoc@hotmail.com>
2022-08-14 16:43:47 -0300

    Fixing recipe not found bug🐞

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -12,7 +12,7 @@ def home(request):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(pk=id, is_published=True)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
