3c0e1e319649c9229315057aca6055c268c02e9b
João Lucas <Jlucasoc@hotmail.com>
2022-11-12 23:55:12 -0300

    Creating first test 🧪

--- first_project/recipes/tests.py
+++ first_project/recipes/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
-# Create your tests here.
 class RecipeURLsTest(TestCase):
-    def test_pytest(self):
-        assert 1 == 1
+    def test_recipe_home_url_is_resolved(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
