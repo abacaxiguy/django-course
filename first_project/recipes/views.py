77ab4f43a0505a68a1ce03517397321d42c4bae1
João Lucas <Jlucasoc@hotmail.com>
2023-02-10 22:19:47 -0300

    Security and page title 🔒

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -36,9 +36,12 @@ def category(request, category_id):
 
 
 def search(request):
-    search_term = request.GET.get("term")
+    search_term = request.GET.get("search", '').strip()
 
     if not search_term:
         raise Http404()
 
-    return render(request, "recipes/pages/search.html")
+    return render(request, "recipes/pages/search.html", {
+        "page_title": f"Search results for '{search_term}'",
+        "search_term": search_term,
+    })
